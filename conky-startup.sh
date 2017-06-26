7d23970048e345ab35fcfa5c5949d0b13ce6f915
prashantjoge <prashantjoge@gmail.com>
2017-06-26 08:56:22 +0530

    a

---
+++ conky-startup.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+## /usr/local/bin
+
+sleep 5s
+killall conky
+# cd "/home/aaron/.conky/Green Apple Desktop"
+conky -c "/home/aaron/.conky/Green Apple Desktop/conky_seamod" &
+conky -c "/home/aaron/.conky/conky_cmd" &
