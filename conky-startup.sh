62147041147092feef4408325f301a37a052ef22
prashant thomas <prashantjoge@gmail.com>
2019-01-11 18:11:15 +0530

    a

--- conky-startup.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-## /usr/local/bin
-sleep 20s
-killall conky
-cd "/home/aaron/.conky/Green Apple Desktop"
-conky -c "/home/aaron/.conky/Green Apple Desktop/conky_seamod" 
-conky -c "/home/aaron/.conky/conky_cmd" 
--- openfile.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-# /usr/local/bin/openfile.sh
-# Script to open files based on the mime types using ROFI
-# This is coupled with existing ROFI script
-
-xdg-open "$(locate home | rofi threads 0  -dmenu -i -p "locate:")"
-
