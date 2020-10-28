eae30358e4e98652a50a562e28a0b05a7bbf268c
Prashant Thomas <prashantjoge@gmail.com>
2020-10-28 22:53:56 +0530

    updated i3block colours

--- data/usr/local/bin/az-104.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-echo $((($(date +%s --date "2020-12-23")-$(date +%s))/(3600*24))) days
--- data/usr/local/bin/az-400.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-echo $((($(date +%s --date "2021-03-24")-$(date +%s))/(3600*24))) days
